173631c33edf9103c7b3ba579562680a3d4fe544
Kyle Chen <kyle.h.chen@gmail.com>
2017-10-07 16:02:49 -0700

    Complete Simon game

--- W2D1/Simon/lib/simon.rb
+++ W2D1/Simon/lib/simon.rb
@@ -10,19 +10,61 @@ class Simon
   end
 
   def play
+    system("clear")
+    puts "Welcome to Simon!"
+    print "Try to repeat the following sequence! It may start off easy,"
+    puts " but it will definitely get harder!"
 
+    5.downto(1).each do |num|
+      print "#{num}... "
+      sleep(1)
+    end
+
+    until game_over
+      take_turn
+    end
+
+    game_over_message
+    reset_game
   end
 
   def take_turn
-    
+    system("clear")
+
+    show_sequence
+    require_sequence
+
+    unless game_over
+      round_success_message
+      sleep(1)
+      @sequence_length += 1
+    end
   end
 
   def show_sequence
+    add_random_color
 
+    seq.each do |color|
+      puts color.upcase
+      sleep(1)
+      system("clear")
+      sleep(0.25)
+    end
+
+    system("clear")
   end
 
   def require_sequence
+    seq.each do |color|
+      system("clear")
+      puts "What's the next color?"
 
+      input = gets.chomp
+      if color != input
+        @game_over = true
+        break
+      end
+    end
   end
 
   def add_random_color
@@ -30,14 +72,20 @@ class Simon
   end
 
   def round_success_message
-
+    puts "Successful round!"
   end
 
   def game_over_message
-
+    puts "That's not right! Game over!"
   end
 
   def reset_game
-
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
   end
+end
+
+if $PROGRAM_NAME == __FILE__
+  Simon.new.play
 end
